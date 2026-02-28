20a671c816dd6a332bf8b553c792925f701e01a0
Don Williams <Don.e.williams@gmail.com>
2026-02-27 20:25:43 -0500

    Adding scrolling to ChangeLayout script
    Requires HL v0.54+
    
     On branch development
     Your branch is up to date with 'origin/development'.
    
     Changes to be committed:
    	modified:   config/hypr/scripts/ChangeLayout.sh

--- config/hypr/scripts/ChangeLayout.sh
+++ config/hypr/scripts/ChangeLayout.sh
@@ -13,11 +13,11 @@ LAYOUT=$(hyprctl -j getoption general:layout | jq '.str' | sed 's/"//g')
 
 # Reverse layout value to reuse toggle logic. So layouts don't get swapped initially.
 if [ "$1" = "init" ]; then
-  if [ "$LAYOUT" = "master" ]; then
-    LAYOUT="dwindle"
-  else
-    LAYOUT="master"
-  fi
+  case "$LAYOUT" in
+  "master") LAYOUT="scrolling" ;;
+  "dwindle") LAYOUT="master" ;;
+  "scrolling") LAYOUT="dwindle" ;;
+  esac
 fi
 
 case $LAYOUT in
@@ -25,18 +25,28 @@ case $LAYOUT in
   hyprctl keyword general:layout dwindle
   hyprctl keyword unbind SUPER,J
   hyprctl keyword unbind SUPER,K
+  hyprctl keyword unbind SUPER,O
   hyprctl keyword bind SUPER,J,cyclenext
   hyprctl keyword bind SUPER,K,cyclenext,prev
   hyprctl keyword bind SUPER,O,layoutmsg,togglesplit
   notify-send -e -u low -i "$notif" " Dwindle Layout"
   ;;
 "dwindle")
-  hyprctl keyword general:layout master
+  hyprctl keyword general:layout scrolling
   hyprctl keyword unbind SUPER,J
   hyprctl keyword unbind SUPER,K
   hyprctl keyword unbind SUPER,O
   hyprctl keyword bind SUPER,J,layoutmsg,cyclenext
   hyprctl keyword bind SUPER,K,layoutmsg,cycleprev
+  notify-send -e -u low -i "$notif" " Scrolling Layout"
+  ;;
+"scrolling")
+  hyprctl keyword general:layout master
+  hyprctl keyword unbind SUPER,J
+  hyprctl keyword unbind SUPER,K
+  hyprctl keyword unbind SUPER,O
+  hyprctl keyword bind SUPER,J,layoutmsg,focus l
+  hyprctl keyword bind SUPER,K,layoutmsg,focus r
   notify-send -e -u low -i "$notif" " Master Layout"
   ;;
 *) ;;
